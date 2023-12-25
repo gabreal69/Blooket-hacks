5a4f4a9072f547a713603afc5140a33ce3fab79e
lucas <153396560+lucasknowsyou@users.noreply.github.com>
2023-12-25 08:12:42 -0500

    seems to work better

--- all-answers-correct.js
+++ all-answers-correct.js
@@ -1,23 +1,15 @@
 Object.defineProperty(Object.prototype, 'state', {
     configurable: true,
     get() {
-        if (this && this.props && this.props.question) {
-            let question = this.props.question
-            question.correctAnswers = question.answers
+        if (this?.props?.question) {
+            this.props.question.correctAnswers = this.props.question.answers;
         }
-        const descriptor = Object.getOwnPropertyDescriptor(this, 'state');
-        return descriptor ? descriptor.value : undefined;
+        return this._state;
     },
     set(value) {
-        if (this && this.props && this.props.question) {
-            let question = this.props.question
-            question.correctAnswers = question.answers
+        if (this?.props?.question) {
+            this.props.question.correctAnswers = this.props.question.answers;
         }
-        Object.defineProperty(this, 'state', {
-            configurable: true,
-            enumerable: true,
-            writable: true,
-            value: value
-        });
+        this._state = value;
     }
 });
